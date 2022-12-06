613fd5fc8c6bc1c053cf65be0925cefcd8e996b3
AliKZ | Win11 <A.koleinyz@gmail.com>
2022-12-06 19:56:57 +0330

    add out-null to commands

--- script.ps1
+++ script.ps1
@@ -1,25 +1,14 @@
-# Prompt the user to enter a value
-# $prompt = "Enter a value"
-
-# Read the user's input and store it in a variable
-# $input = Read-Host -Prompt $prompt
-
-
 # Add all changes in the repo to the staging area
 Write-Host "✔ Add all changes in the your repo to the staging area..."
-git add -A
+git add -A | Out-Null
 
 # git commit
 $message = Read-Host "🖊 Tell me a specified commit message"
-git commit -m $message
-
-# $prompt = "🖊 Tell me a specified commit message"
-# $input = Read-Host -Prompt $message
-# git commit -m "$message"
+git commit -m $message | Out-Null
 
 # Push the changes to the remote repository
-Write-Host "⬆ Push the changes to the remote repository"
-git push -u origin main
+Write-Host "⬆ Push the changes to the remote repository..."
+git push -u origin main | Out-Null
 
 # Display the Git log
 git log --pretty=oneline --abbrev-commit --decorate --graph -n 5
